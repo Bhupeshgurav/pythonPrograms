126b03b154f2d733aa1f0afae9c45fec55d796f9
Bhupesh Gurav <bhupeshgurav124@gmail.com>
2022-05-18 15:30:52 +0530

    increasingDecreasing

---
+++ increasingDecreasing.py
@@ -0,0 +1,10 @@
+
+def check(numList):
+    if sorted(set(numList)) == numList:
+        return "increasing"
+    if sorted(set(numList), reverse=True) == numList:
+        return "decreasing"
+    return "neither"
+
+
+print(check([1, 2, 3]))
