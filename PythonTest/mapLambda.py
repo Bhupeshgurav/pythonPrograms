121032b4330eb61de10eb2cd06490fc0573c9019
Bhupesh Gurav <bhupeshgurav124@gmail.com>
2022-05-20 13:12:20 +0530

    filter

--- PythonTest/mapLambda.py
+++ PythonTest/mapLambda.py
@@ -18,3 +18,26 @@ lst1 = [-1, -2, 1, 2, -5, 5, -6, -66, 1, 2, 3, 10]
 # sol2
 result = list(map(abs, lst1))
 print(result)
+
+# q3
+
+country = ["mumbai", "pune", "delhi", "goa", "nashik"]
+# output  [['m', 'u', 'm', 'b', 'a', 'i'], ['p', 'u', 'n', 'e'], ['d',
+# 'e', 'l', 'h', 'i'], ['g', 'o', 'a'], ['n', 'a', 's', 'h', 'i', 'k']]
+
+
+def inner(a):
+    b = []
+    for i in a:
+        b.append(i)
+    return b
+
+
+result = list(map(inner, country))
+print(result)
+
+# ages = [10,15,41]
+# age>18
+
+result = list(filter(lambda x: True if x > 18 else False, ages))
+print(result)
